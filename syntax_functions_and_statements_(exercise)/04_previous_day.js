79c18803e10fd30989c496c0f6045761246e6edf
Peter Zhechev <petar_vn7@abv.bg>
2023-04-05 22:21:56 +0300

    Update 04_previous_day.js

--- syntax_functions_and_statements_(exercise)/04_previous_day.js
+++ syntax_functions_and_statements_(exercise)/04_previous_day.js
@@ -1,6 +1,14 @@
 function previousDay(year, month, day) {
-    
 
+    let now = new Date(year, month - 1, day - 1);
+
+    let currentDate = now.getDate();
+    let currentMonth = now.getMonth();
+    let currentYear = now.getFullYear();
+
+    console.log(`${currentYear}-${currentMonth + 1}-${currentDate}`);
 }
 
 previousDay(2016, 9, 30);
+
+previousDay(2023, 1, 1);
