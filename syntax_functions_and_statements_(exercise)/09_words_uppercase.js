cb1dff81c48ee97592ed50704bd364852f965dbd
Peter Zhechev <petar_vn7@abv.bg>
2023-04-07 18:05:17 +0300

    added two new tasks

---
+++ syntax_functions_and_statements_(exercise)/09_words_uppercase.js
@@ -0,0 +1,17 @@
+function wordsUppercase(inputText) {
+    
+    let pattern = /(?<word>[A-Za-z]+)/g;
+    let mattchedWords = [...inputText.matchAll(pattern)];
+    let extractedWords = [];
+
+    for (let word of mattchedWords) {
+        let upperCaseWord = word.groups.word.toUpperCase();
+        extractedWords.push(upperCaseWord);
+    }
+
+    console.log(extractedWords.join(', '));
+}
+
+wordsUppercase('Hi, how are you?');
+
+wordsUppercase('hello');
