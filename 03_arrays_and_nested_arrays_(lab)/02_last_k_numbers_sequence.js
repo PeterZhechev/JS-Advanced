ac31fae330b070be4397fbcec08cea5c5430dd09
Peter Zhechev <118084167+PeterZhechev@users.noreply.github.com>
2023-04-08 14:29:14 +0300

    Update 02_last_k_numbers_sequence.js

--- 03_arrays_and_nested_arrays_(lab)/02_last_k_numbers_sequence.js
+++ 03_arrays_and_nested_arrays_(lab)/02_last_k_numbers_sequence.js
@@ -1,6 +1,26 @@
-function lastKNumbersSequence(params) {
-    
-    
+function lastKNumbersSequence(nNumber, kNumber) {
+
+    let nums = [1];
+    let count = kNumber;
+
+    for (let i = 1; i < nNumber; i++) {
+        let currentNum = 0;
+        let currentNums = 0;
+        if (nums.length <= kNumber) {
+            count = nums.length;
+        } else {
+            count = kNumber;
+        }
+
+        for (let index = 1; index <= count; index++) {
+            currentNum = nums[nums.length - index];
+            currentNums += currentNum;
+        }
+
+        nums.push(currentNums);
+    }
+
+    return nums
 }
 
-lastKNumbersSequence();
+lastKNumbersSequence(9, 2);
