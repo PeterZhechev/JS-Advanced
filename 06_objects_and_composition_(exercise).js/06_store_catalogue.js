b2c5ce8699585db10a671bcfdf47f0e064877285
Peter Zhechev <petar_vn7@abv.bg>
2023-04-15 14:39:35 +0300

    Create 06_store_catalogue.js

---
+++ 06_objects_and_composition_(exercise).js/06_store_catalogue.js
@@ -0,0 +1,15 @@
+function storeCatalogue(params) {
+
+}
+
+storeCatalogue([
+    'Appricot : 20.4',
+    'Fridge : 1500',
+    'TV : 1499',
+    'Deodorant : 10',
+    'Boiler : 300',
+    'Apple : 1.25',
+    'Anti-Bug Spray : 15',
+    'T-Shirt : 10'
+]);
+
