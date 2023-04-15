72c12cb0b4ec5a61a5dbd5ab3561282bd59de794
Peter Zhechev <petar_vn7@abv.bg>
2023-04-15 10:43:58 +0300

    Update 04_heroic_inventory.js

--- 06_objects_and_composition_(exercise).js/04_heroic_inventory.js
+++ 06_objects_and_composition_(exercise).js/04_heroic_inventory.js
@@ -1,5 +1,44 @@
-function heroicInventory(params) {
-    
+function heroicInventory(arr) {
+
+    const heroes = [];
+
+    for (let hero of arr) {
+        let splitted = hero.split(' / ');
+
+        let heroName = splitted.shift();
+        let heroLevel = '';
+
+        if (splitted.length >= 1) {
+            heroLevel = splitted.shift();
+        }
+
+        let items = '';
+        if (splitted.length >= 1) {
+            items = splitted.shift().split(', ');
+        }
+
+        const currentHero = {
+            name: heroName,
+            level: Number(heroLevel),
+            items: [],
+        };
+
+        for (let item of items) {
+            currentHero.items.push(item);
+        }
+
+        heroes.push(currentHero);
+
+    }
+
+    return JSON.stringify(heroes);
 }
 
-heroicInventory();
+console.log(heroicInventory([
+    'Isacc / 25 / Apple',
+    'Derek / 12 / BarrelVest, DestructionSword',
+    'Hes / 1 / Desolator, Sentinel, Antara'
+]));
+
+
+[{ "name": "Isacc", "level": 25, "items": ["Apple", "GravityGun"] }, { "name": "Derek", "level": 12, "items": ["BarrelVest", "DestructionSword"] }, { "name": "Hes", "level": 1, "items": ["Desolator", "Sentinel", "Antara"] }]
