364e10620378eb1e0eb0d954d8758d88fe3dd277
Peter Zhechev <petar_vn7@abv.bg>
2023-04-16 20:45:17 +0300

    Update 06_store_catalogue.js

--- 06_objects_and_composition_(exercise).js/06_store_catalogue.js
+++ 06_objects_and_composition_(exercise).js/06_store_catalogue.js
@@ -1,5 +1,26 @@
-function storeCatalogue(params) {
+function storeCatalogue(arr) {
 
+    let productList = new Map();
+    let sortedArr = arr.sort((a, b) => a.localeCompare(b));
+
+    for (let line of sortedArr) {
+
+        let [productName, productPrice] = line.split(' : ');
+        let currentProductGroup = productName[0];
+        if (!productList.has(currentProductGroup)) {
+            productList.set(currentProductGroup, new Map());
+            productList.get(currentProductGroup).set(productName, Number(productPrice));
+        } else {
+            productList.get(currentProductGroup).set(productName, Number(productPrice));
+        }
+    }
+
+    for (let group of productList) {
+        console.log(group[0]);
+        for (let product of group[1]) {
+            console.log(`  ${product[0]}: ${product[1]}`);
+        }
+    }
 }
 
 storeCatalogue([
