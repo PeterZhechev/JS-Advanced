c9fccde73b3a45ed3e3683ca8b2cc31fe49fcc96
Peter Zhechev <petar_vn7@abv.bg>
2023-04-17 11:04:01 +0300

    updated 3 tasks

--- 07_dom_introduction_(lab)/01.Edit Element/EditElement.js
+++ 07_dom_introduction_(lab)/01.Edit Element/EditElement.js
@@ -1,3 +1,10 @@
-function editElement() {
-    // TODO
+function editElement(ref, match, replacer) {
+
+    ref.textContent = ref.textContent.split(match).join(replacer);
+
+    // const text = ref.textContent;
+
+    // const result = text.split(match).join(replacer);
+
+    // ref.textContent = result;
 }
