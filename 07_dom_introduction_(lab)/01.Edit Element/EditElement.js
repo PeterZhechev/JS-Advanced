bfe573e4c9bbfd70edddee827b9b1794d00ccc7f
Peter Zhechev <petar_vn7@abv.bg>
2023-04-17 11:04:32 +0300

    Update EditElement.js

--- 07_dom_introduction_(lab)/01.Edit Element/EditElement.js
+++ 07_dom_introduction_(lab)/01.Edit Element/EditElement.js
@@ -2,9 +2,4 @@ function editElement(ref, match, replacer) {
 
     ref.textContent = ref.textContent.split(match).join(replacer);
 
-    // const text = ref.textContent;
-
-    // const result = text.split(match).join(replacer);
-
-    // ref.textContent = result;
 }
