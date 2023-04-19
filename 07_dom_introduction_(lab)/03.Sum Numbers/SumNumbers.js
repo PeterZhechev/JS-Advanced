1e7bcf262a99cb69482f728698046f52245b9ce2
Peter Zhechev <petar_vn7@abv.bg>
2023-04-19 18:46:04 +0300

    update 2 tasks

--- 07_dom_introduction_(lab)/03.Sum Numbers/SumNumbers.js
+++ 07_dom_introduction_(lab)/03.Sum Numbers/SumNumbers.js
@@ -1,3 +1,12 @@
 function calc() {
-    // TODO: sum = num1 + num2
+    let firstNum = document.getElementById('num1');
+    let secondNum = document.getElementById('num2');
+    let valueOfFirstNum = firstNum.value;
+    let valueOfSecondNum = secondNum.value;
+
+    let sum = Number(valueOfFirstNum) + Number(valueOfSecondNum);
+
+    let result = document.getElementById('sum');
+
+    result.value = sum;
 }
