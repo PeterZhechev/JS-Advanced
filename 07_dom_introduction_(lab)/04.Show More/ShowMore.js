ae4b11de3a913447bfb634545bf8dbac474117b6
Peter Zhechev <petar_vn7@abv.bg>
2023-04-19 19:48:43 +0300

    Update ShowMore.js

--- 07_dom_introduction_(lab)/04.Show More/ShowMore.js
+++ 07_dom_introduction_(lab)/04.Show More/ShowMore.js
@@ -1,3 +1,4 @@
 function showText() {
-    // TODO
+    const text = document.getElementById('text').style.display = 'inline';
+    const readMe = document.getElementById('more').style.display = "none";
 }
