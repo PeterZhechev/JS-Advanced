f665d4831ca5162ec77b4298bc4321fd228066fc
Peter Zhechev <petar_vn7@abv.bg>
2023-04-24 21:43:06 +0300

    Update SumTable.js

--- 07_dom_introduction_(lab)/06.Sum Table/SumTable.js
+++ 07_dom_introduction_(lab)/06.Sum Table/SumTable.js
@@ -1,3 +1,16 @@
 function sumTable() {
+    const prices = document.querySelectorAll('table tr td');
+    let sum = prices[prices.length - 1];
+    let total = 0;
+    let index = 1;
 
+    for (let price of prices) {
+        if (index % 2 === 0) {
+            total += Number(price.textContent);
+        }
+
+        index++;
+    }
+
+    sum.innerHTML = total;
 }
