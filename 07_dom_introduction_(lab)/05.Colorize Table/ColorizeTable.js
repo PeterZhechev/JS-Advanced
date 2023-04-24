a8aa77af60b6fd5de9bf4cd183bc1c26459740ec
Peter Zhechev <petar_vn7@abv.bg>
2023-04-24 21:09:05 +0300

    Update ColorizeTable.js

--- 07_dom_introduction_(lab)/05.Colorize Table/ColorizeTable.js
+++ 07_dom_introduction_(lab)/05.Colorize Table/ColorizeTable.js
@@ -2,9 +2,9 @@ function colorize() {
 
     let rowElements = document.querySelectorAll('table tr');
 
-    for (let i = 0; i < rowElements.length; i++) {
-        if (i % 2 !== 0) {
-            rowElements[i].style.backgroundColor = 'teal';
+    for (let i = 1; i <= rowElements.length; i++) {
+        if (i % 2 === 0) {
+            rowElements[i - 1].style.backgroundColor = 'teal';
         }
     }
 }
