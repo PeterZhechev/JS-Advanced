032cf60fc90b90af2d3d6ed377718cb957236536
Peter Zhechev <petar_vn7@abv.bg>
2023-04-24 23:10:38 +0300

    Update search.js

--- 08_dom_introduction_(exercise)/04. Search in List/search.js
+++ 08_dom_introduction_(exercise)/04. Search in List/search.js
@@ -1,3 +1,28 @@
 function search() {
-   // TODO
+
+   const list = document.querySelectorAll('#towns li');
+   let matches = 0;
+   let input = document.getElementById('searchText').value;
+
+   for (let line of list) {
+
+      line.style.fontWeight = "normal";
+      line.style.textDecoration = "none";
+   }
+
+   for (let line of list) {
+
+      let currentWord = line.innerHTML;
+
+      if (currentWord.includes(input) && input !== '') {
+         line.style.fontWeight = "bold";
+         line.style.textDecoration = "underline";
+         matches++;
+      }
+   }
+
+   document.getElementById('result').textContent = `${matches} matches found`;
+
+   document.getElementById('searchText').value = '';
+   matches = 0;
 }
