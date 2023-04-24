53cfc90084caccec6c369c7d975a88a31ab66e8a
Peter Zhechev <petar_vn7@abv.bg>
2023-04-24 21:58:57 +0300

    Update ExtractParenthesis.js

--- 07_dom_introduction_(lab)/07.Extract Parenthesis/ExtractParenthesis.js
+++ 07_dom_introduction_(lab)/07.Extract Parenthesis/ExtractParenthesis.js
@@ -1,3 +1,12 @@
 function extract(content) {
+    let text = document.getElementById('content').innerText;
+    let pattern = /\((?<word>[A-Za-z0-9 ]+)\)/g;
+    let matched = [...text.matchAll(pattern)];
+    let arr = [];
 
+    for (let match of matched) {
+        arr.push(match.groups['word']);
+    }
+
+    return arr.join('; ');
 }
