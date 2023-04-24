2e2be2fa9194727e4ddfee1ce09fd71f9815f65c
Peter Zhechev <118084167+PeterZhechev@users.noreply.github.com>
2023-04-24 18:14:46 +0300

    Update solution.js

--- 08_dom_introduction_(exercise)/02. Pascal or Camel Case/solution.js
+++ 08_dom_introduction_(exercise)/02. Pascal or Camel Case/solution.js
@@ -1,3 +1,48 @@
 function solve() {
-  //TODO...
+  let input = document.getElementById('text').value;
+  let currentCase = document.getElementById('naming-convention').value;
+  let outputText = 'Error!';
+
+  if (currentCase === 'Camel Case') {
+    outputText = camelCase(input)
+  } else if (currentCase === 'Pascal Case') {
+    outputText = pascalCase(input);
+  }
+
+  function camelCase(inputText) {
+    let outputText = inputText.toLowerCase();
+    let arr = outputText.split(' ');
+    let result = [];
+    result.push(arr[0]);
+
+    for (let i = 1; i < arr.length; i++) {
+      let currentWord = arr[i];
+      if (currentWord === '') continue;
+      let currentLetter = currentWord[0].toUpperCase();
+      let currentWordArr = currentWord.split('');
+      currentWordArr.splice(0, 1, currentLetter);
+      result.push(currentWordArr.join(''));
+    }
+
+    return result.join('');
+  }
+
+  function pascalCase(inputText) {
+    let outputText = inputText.toLowerCase();
+    let arr = outputText.split(' ');
+    let result = [];
+
+    for (let i = 0; i < arr.length; i++) {
+      let currentWord = arr[i];
+      if (currentWord === '') continue;
+      let currentLetter = currentWord[0].toUpperCase();
+      let currentWordArr = currentWord.split('');
+      currentWordArr.splice(0, 1, currentLetter);
+      result.push(currentWordArr.join(''));
+    }
+
+    return result.join('');
+  }
+
+  document.getElementById('result').innerHTML = outputText;
 }
