7545f74507449978c27f0470a42ad059097cd2fb
Peter Zhechev <petar_vn7@abv.bg>
2023-04-28 18:00:16 +0300

    Update solution.js

--- 08_dom_introduction_(exercise)/05. Table - Search Engine/solution.js
+++ 08_dom_introduction_(exercise)/05. Table - Search Engine/solution.js
@@ -2,7 +2,18 @@ function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
    function onClick() {
-      //   TODO:
+      let input = document.querySelector('#searchField');
+      let items = Array.from(document.querySelectorAll('.container > tbody > tr'));
 
+      for (let item of items) {
+         // console.log(item.textContent);
+         if (item.textContent.includes(input.value) && input.value !== '') {
+            item.className = 'select';
+         } else {
+            item.classList.remove('select');
+         }
+      }
+
+      input = document.querySelector('#searchField').value = '';
    }
 }
