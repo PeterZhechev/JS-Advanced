7f8c3c67d8ec56da940d8ae4421ee3ff3c195f5f
Peter Zhechev <petar_vn7@abv.bg>
2023-04-29 12:42:15 +0300

    Update solution.js

--- 08_dom_introduction_(exercise)/06. Format the Text/solution.js
+++ 08_dom_introduction_(exercise)/06. Format the Text/solution.js
@@ -1,3 +1,36 @@
 function solve() {
-  //TODO
+
+  let inputArea = document.getElementById('input');
+  let outputArea = document.getElementById('output');
+  const pattern = /(?<sentence>[^.]+)/g;
+  let outputArr = [];
+  let count = 1;
+  let currentParagraph = [];
+
+  let matched = [...inputArea.value.matchAll(pattern)];
+
+  for (let match of matched) {
+    let splitted = match.groups.sentence.split('')
+    splitted.push('.');
+    let joinned = splitted.join('');
+
+    if (count % 3 !== 0) {
+      currentParagraph.push(joinned);
+    } else {
+      currentParagraph.push(joinned);
+      let outputParagraph = currentParagraph.join('');
+      outputArr.push(`<p>${outputParagraph}</p>`);
+      currentParagraph = [];
+      outputParagraph = [];
+    }
+
+    count++;
+  }
+
+  if (currentParagraph.length > 0) {
+    let outputParagraph = currentParagraph.join('');
+    outputArr.push(`<p>${outputParagraph}</p>`);
+  }
+
+  outputArea.innerHTML = outputArr.join('');
 }
