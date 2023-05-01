91d796664a9e0eb122c9bc71e6edfd0d630895ef
Peter Zhechev <petar_vn7@abv.bg>
2023-05-01 18:35:51 +0300

    Update report.js

--- 08_dom_introduction_(exercise)/08. Generate Report/report.js
+++ 08_dom_introduction_(exercise)/08. Generate Report/report.js
@@ -2,25 +2,33 @@ function generateReport() {
 
     let checkBoxesHead = Array.from(document.querySelector('body > main > table > thead > tr').getElementsByTagName('th'));
     let checkBoxesBody = Array.from(document.querySelector('body > main > table > tbody').getElementsByTagName('tr'));
-
+    let outputArr = [];
 
     for (let index = 0; index < checkBoxesHead.length; index++) {
         let box = checkBoxesHead[index];
 
             if (box.querySelector('input').checked) {
-                
-                for (let el of checkBoxesBody) {
-                    let currentRow = Array.from(el.getElementsByTagName('td'));
+                let columnName = box.querySelector('input').name;
+                let currentObj = {};
+
+                for (let j = 0; j < checkBoxesBody.length; j++) {
+                    let currentEl = checkBoxesBody[j];
+                    let currentRow = Array.from(currentEl.getElementsByTagName('td'));
                     let currentRowText = currentRow[index].textContent;
-                    console.log(currentRowText);
+                    currentObj[columnName] = currentRowText;
+
+                    if (!outputArr[j]) {
+                        outputArr.push(currentObj);
+                        currentObj = {};
+                    } else {
+                        outputArr[j][columnName] = currentRowText;
+                    }
                 }
 
             }
 
-        
-
     }
 
     const outputArea = document.querySelector('#output');
-
+    outputArea.value = JSON.stringify(outputArr);
 }
