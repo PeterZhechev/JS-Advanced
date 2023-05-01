d4d222e42eaa38012cddaedbb89fe99090fabfa3
Peter Zhechev <118084167+PeterZhechev@users.noreply.github.com>
2023-05-01 13:14:50 +0300

    Update report.js

--- 08_dom_introduction_(exercise)/08. Generate Report/report.js
+++ 08_dom_introduction_(exercise)/08. Generate Report/report.js
@@ -1,3 +1,8 @@
 function generateReport() {
-    //TODO
+    let employeeCheck = document.querySelector('body > main > table > thead > tr > th:nth-child(1) > input[type=checkbox]').checked;
+    let departmentCheck = document.querySelector('body > main > table > thead > tr > th:nth-child(2) > input[type=checkbox]').checked;
+    let statusCheck = document.querySelector('body > main > table > thead > tr > th:nth-child(3) > input[type=checkbox]').checked;
+    let dateHiredCheck = document.querySelector('body > main > table > thead > tr > th:nth-child(4) > input[type=checkbox]').checked;
+    let benefitsCheck = document.querySelector('body > main > table > thead > tr > th:nth-child(5) > input[type=checkbox]').checked;
+    
 }
