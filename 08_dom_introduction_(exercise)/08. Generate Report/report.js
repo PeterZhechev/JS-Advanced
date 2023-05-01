efdfcf9a3fb8a2ffb32080d575dcf12f930898ab
Peter Zhechev <petar_vn7@abv.bg>
2023-05-01 18:18:30 +0300

    Update report.js

--- 08_dom_introduction_(exercise)/08. Generate Report/report.js
+++ 08_dom_introduction_(exercise)/08. Generate Report/report.js
@@ -1,70 +1,26 @@
 function generateReport() {
-    let employeeCheck = document.querySelector('body > main > table > thead > tr > th:nth-child(1) > input[type=checkbox]').checked;
-    let departmentCheck = document.querySelector('body > main > table > thead > tr > th:nth-child(2) > input[type=checkbox]').checked;
-    let statusCheck = document.querySelector('body > main > table > thead > tr > th:nth-child(3) > input[type=checkbox]').checked;
-    let dateHiredCheck = document.querySelector('body > main > table > thead > tr > th:nth-child(4) > input[type=checkbox]').checked;
-    let benefitsCheck = document.querySelector('body > main > table > thead > tr > th:nth-child(5) > input[type=checkbox]').checked;
-    let compensationCheck = document.querySelector('body > main > table > thead > tr > th:nth-child(6) > input[type=checkbox]').checked;
-    let ratingCheck = document.querySelector('body > main > table > thead > tr > th:nth-child(7) > input[type=checkbox]').checked;
 
-    let table = Array.from(document.querySelectorAll('body > main > table > tbody tr'));
-    let reportArr = [];
-    let count = 1;
-    let rowsCount = 0;
-    let isObj = false;
+    let checkBoxesHead = Array.from(document.querySelector('body > main > table > thead > tr').getElementsByTagName('th'));
+    let checkBoxesBody = Array.from(document.querySelector('body > main > table > tbody').getElementsByTagName('tr'));
 
-    if (employeeCheck) {
-        count = 1;
-        rowsCount = 0;
-        if (reportArr.length === 0) {
-            isObj = true;
-        } else {
-            isObj = false;
-        }
 
-        for (let row of table) {
-            let currentEmployeeColumn = document.querySelector(`body > main > table > tbody > tr:nth-child(${count}) > td:nth-child(1)`);
+    for (let index = 0; index < checkBoxesHead.length; index++) {
+        let box = checkBoxesHead[index];
 
-            if (isObj) {
-                reportArr[rowsCount] = {};
+            if (box.querySelector('input').checked) {
+                
+                for (let el of checkBoxesBody) {
+                    let currentRow = Array.from(el.getElementsByTagName('td'));
+                    let currentRowText = currentRow[index].textContent;
+                    console.log(currentRowText);
+                }
+
             }
 
-            reportArr[rowsCount].employee = currentEmployeeColumn.textContent;
-            count++;
-            rowsCount++;
-        }
-    }
-
-    if (departmentCheck) {
-        count = 1;
-        rowsCount = 0;
-        if (reportArr.length === 0) {
-            isObj = true;
-        } else {
-            isObj = false;
-        }
-
-        for (let row of table) {
-            let currentDepartmentColumn = document.querySelector(`body > main > table > tbody > tr:nth-child(${count}) > td:nth-child(2)`);
-
-            if (isObj) {
-                reportArr[rowsCount] = {};
-            }
-
-            if (reportArr.length === 0) {
-                reportArr[rowsCount] = {};
-            }
-
-            reportArr[rowsCount].department = currentDepartmentColumn.textContent;
-            count++;
-            rowsCount++;
-        }
-    }
-
-    if (statusCheck) {
+        
 
     }
 
-    document.querySelector('#output').value = JSON.stringify(reportArr);
-    console.log(JSON.stringify(reportArr));
+    const outputArea = document.querySelector('#output');
+
 }
