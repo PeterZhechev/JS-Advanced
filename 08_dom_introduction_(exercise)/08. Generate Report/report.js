f3a6bfbf7dc7ed9df661c07c6e92c9c051a003e0
Peter Zhechev <118084167+PeterZhechev@users.noreply.github.com>
2023-05-01 14:56:13 +0300

    Update report.js

--- 08_dom_introduction_(exercise)/08. Generate Report/report.js
+++ 08_dom_introduction_(exercise)/08. Generate Report/report.js
@@ -8,26 +8,63 @@ function generateReport() {
     let ratingCheck = document.querySelector('body > main > table > thead > tr > th:nth-child(7) > input[type=checkbox]').checked;
 
     let table = Array.from(document.querySelectorAll('body > main > table > tbody tr'));
-    let reportObj = [];
+    let reportArr = [];
+    let count = 1;
+    let rowsCount = 0;
+    let isObj = false;
 
     if (employeeCheck) {
-        let employeeObj = {};
-        let count = 1;
-        for(let row of table) {
-            console.log(document.querySelector(`body > main > table > tbody > tr:nth-child(${count}) > td:nth-child(1)`));
-
-            count++;
+        count = 1;
+        rowsCount = 0;
+        if (reportArr.length === 0) {
+            isObj = true;
+        } else {
+            isObj = false;
         }
-       
+
+        for (let row of table) {
+            let currentEmployeeColumn = document.querySelector(`body > main > table > tbody > tr:nth-child(${count}) > td:nth-child(1)`);
+
+            if (isObj) {
+                reportArr[rowsCount] = {};
+            }
+
+            reportArr[rowsCount].employee = currentEmployeeColumn.textContent;
+            count++;
+            rowsCount++;
+        }
     }
 
     if (departmentCheck) {
+        count = 1;
+        rowsCount = 0;
+        if (reportArr.length === 0) {
+            isObj = true;
+        } else {
+            isObj = false;
+        }
 
+        for (let row of table) {
+            let currentDepartmentColumn = document.querySelector(`body > main > table > tbody > tr:nth-child(${count}) > td:nth-child(2)`);
+
+            if (isObj) {
+                reportArr[rowsCount] = {};
+            }
+
+            if (reportArr.length === 0) {
+                reportArr[rowsCount] = {};
+            }
+
+            reportArr[rowsCount].department = currentDepartmentColumn.textContent;
+            count++;
+            rowsCount++;
+        }
     }
 
     if (statusCheck) {
 
     }
 
-    console.log(reportObj);
+    document.querySelector('#output').value = JSON.stringify(reportArr);
+    console.log(JSON.stringify(reportArr));
 }
