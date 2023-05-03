2f2d84a8e8ba1e9e310eedfe4fbf9f0e2f1ec324
Peter Zhechev <petar_vn7@abv.bg>
2023-05-03 15:59:24 +0300

    Update solution.js

--- 08_dom_introduction_(exercise)/09. Number Convertor/solution.js
+++ 08_dom_introduction_(exercise)/09. Number Convertor/solution.js
@@ -4,25 +4,25 @@ function solve() {
     optnBinary.textContent = 'Binary';
     optnBinary.value = 'binary';
     document.getElementById('selectMenuTo').appendChild(optnBinary);
-    
+
     const optnHex = document.createElement('option');
     optnHex.textContent = 'Hexadecimal';
     optnHex.value = 'hexadecimal';
     document.getElementById('selectMenuTo').appendChild(optnHex);
 
-    const resultField = document.getElementById('result');
-
     const menu = document.querySelector('#selectMenuTo');
-    
     const btn = document.querySelector('#container > button').addEventListener("click", cnvrtFunc);
+    const input = document.getElementById('input');
+    const resultField = document.getElementById('result');
 
     function cnvrtFunc() {
 
+        let inputNum = Number(input.value);
+
         if (menu.value === 'binary') {
-
+            resultField.value = inputNum.toString(2);
         } else if (menu.value === 'hexadecimal') {
-            
+            resultField.value = inputNum.toString(16).toUpperCase();
         }
-
     }
 }
