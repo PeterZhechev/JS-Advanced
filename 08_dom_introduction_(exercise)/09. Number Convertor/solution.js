a8fca0aae5983366859491b25a5b042d45e919ba
Peter Zhechev <petar_vn7@abv.bg>
2023-05-03 15:46:31 +0300

    Update solution.js

--- 08_dom_introduction_(exercise)/09. Number Convertor/solution.js
+++ 08_dom_introduction_(exercise)/09. Number Convertor/solution.js
@@ -1,4 +1,28 @@
 function solve() {
 
-    //TODO...
+    const optnBinary = document.createElement('option');
+    optnBinary.textContent = 'Binary';
+    optnBinary.value = 'binary';
+    document.getElementById('selectMenuTo').appendChild(optnBinary);
+    
+    const optnHex = document.createElement('option');
+    optnHex.textContent = 'Hexadecimal';
+    optnHex.value = 'hexadecimal';
+    document.getElementById('selectMenuTo').appendChild(optnHex);
+
+    const resultField = document.getElementById('result');
+
+    const menu = document.querySelector('#selectMenuTo');
+    
+    const btn = document.querySelector('#container > button').addEventListener("click", cnvrtFunc);
+
+    function cnvrtFunc() {
+
+        if (menu.value === 'binary') {
+
+        } else if (menu.value === 'hexadecimal') {
+            
+        }
+
+    }
 }
