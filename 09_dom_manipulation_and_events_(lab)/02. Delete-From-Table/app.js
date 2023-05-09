43daa13247830adef2511b974132874f74a50ade
Peter Zhechev <petar_vn7@abv.bg>
2023-05-09 16:51:05 +0300

    Update app.js

--- 09_dom_manipulation_and_events_(lab)/02. Delete-From-Table/app.js
+++ 09_dom_manipulation_and_events_(lab)/02. Delete-From-Table/app.js
@@ -1,3 +1,32 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+    let inputEmailElement = document.getElementsByName('email')[0].value;
+    let tableElements = Array.from(document.querySelector("#customers").querySelectorAll('tr td'));
+    let index = 1
+    let emailsInTableElements = [];
+
+    for (let el of tableElements) {
+        if (index % 2 === 0) {
+            emailsInTableElements.push(el);
+        }
+
+        index++;
+    }
+
+    let isDeleted = false;
+
+    for (let el of emailsInTableElements) {
+        if (el.textContent === inputEmailElement) {
+            el.parentNode.parentNode.removeChild(el.parentNode);
+            document.querySelectorAll('#result')[0].textContent = 'Deleted.';
+            isDeleted = true;
+            break;
+        }
+    }
+
+    if (isDeleted === false) {
+        document.querySelectorAll('#result')[0].textContent = 'Not found.';
+
+    }
+
+    inputEmailElement = document.getElementsByName('email')[0].value = '';
 }
