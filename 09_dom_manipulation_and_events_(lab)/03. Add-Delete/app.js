cb937ea530aa111023d22cf0dd95bda84470c02f
Peter Zhechev <petar_vn7@abv.bg>
2023-05-10 22:45:19 +0300

    Update app.js

--- 09_dom_manipulation_and_events_(lab)/03. Add-Delete/app.js
+++ 09_dom_manipulation_and_events_(lab)/03. Add-Delete/app.js
@@ -1,3 +1,21 @@
 function addItem() {
-    //TODO...
+    let inputAreaElement = document.getElementById('newItemText');
+    let listItemsElement = document.getElementById('items');
+
+    let newLiElement = document.createElement('li');
+    newLiElement.textContent = inputAreaElement.value;
+
+    let deleteButtonElement = document.createElement('a');
+    deleteButtonElement.setAttribute('href', '#');
+    deleteButtonElement.textContent = '[Delete]';
+    deleteButtonElement.addEventListener('click', e => {
+        e.currentTarget.parentNode.remove();
+    });
+
+    newLiElement.appendChild(deleteButtonElement);
+
+    listItemsElement.appendChild(newLiElement);
+
+    inputAreaElement = document.getElementById('newItemText').value = '';
 }
+
