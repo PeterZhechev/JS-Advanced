63796ae27c7fbfdb5be04625227fc734e267e086
Peter Zhechev <petar_vn7@abv.bg>
2023-05-16 20:36:43 +0300

    Update app.js

--- 09_dom_manipulation_and_events_(lab)/06. Dynamic-Validation/app.js
+++ 09_dom_manipulation_and_events_(lab)/06. Dynamic-Validation/app.js
@@ -1,3 +1,21 @@
 function validate() {
-    console.log('TODO:...');
+    let inputElement = document.getElementById('email');
+    inputElement.addEventListener('change', blurFunc);
+
+    function blurFunc() {
+        emailValidator(inputElement.value);
+    }
+
+    function emailValidator(input) {
+        let pattern = /[a-z]+\@[a-z]+\.[a-z]+/g;
+        let matched = input.match(pattern);
+
+        if (inputElement.value !== '') {
+            if (matched) {
+                inputElement.className = '';
+            } else {
+                inputElement.className = 'error';
+            }
+        }
+    }
 }
