440871b084169ebc648aa34f9c74306af10078c1
Peter Zhechev <petar_vn7@abv.bg>
2023-05-16 16:51:48 +0300

    updated two tasks

--- 09_dom_manipulation_and_events_(lab)/04. Mouse-Gradient/app.js
+++ 09_dom_manipulation_and_events_(lab)/04. Mouse-Gradient/app.js
@@ -1,3 +1,15 @@
 function attachGradientEvents() {
-    console.log('TODO:...');
+    let gradient = document.getElementById('gradient');
+    gradient.addEventListener('mousemove', funcMove);
+    gradient.addEventListener('mouseout', funcOut);
+
+    function funcMove(e) {
+        let power = e.offsetX / (e.target.clientWidth - 1);
+        power = Math.trunc(power * 100);
+        document.getElementById('result').textContent = power + "%";    
+    }
+
+    function funcOut() {
+        document.getElementById('result').textContent = "";
+    }
 }
--- 09_dom_manipulation_and_events_(lab)/05. Highlight-Active/app.js
+++ 09_dom_manipulation_and_events_(lab)/05. Highlight-Active/app.js
@@ -1,3 +1,19 @@
 function focused() {
-    console.log('TODO:...');
+
+    let divElement = document.querySelector('div');
+    let divElements = Array.from(divElement.children);
+    
+    for (let el of divElements) {
+        let inputEl = el.children[1];
+        inputEl.addEventListener('focus', focusFunc);
+        inputEl.addEventListener('blur', blurFunc);
+
+        function focusFunc(e) {
+            el.className = 'focused';
+        }
+
+        function blurFunc(e) {
+            el.className = '';
+        }
+    }
 }
