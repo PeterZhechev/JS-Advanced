4cc55778c6a7e020f2a26663ccca24d2a590c552
Peter Zhechev <petar_vn7@abv.bg>
2023-05-18 18:45:38 +0300

    Update solution.js

--- 09_dom_manipulation_and_events_(lab)/07. Shopping-Cart/solution.js
+++ 09_dom_manipulation_and_events_(lab)/07. Shopping-Cart/solution.js
@@ -1,3 +1,53 @@
 function solve() {
-   //TODO...
+   let productsElements = document.querySelector('.shopping-cart');
+   productsElements.addEventListener('click', addFunc);
+   let totalPrice = 0;
+   let allProducts = new Set();
+   let allProductsArr = [];
+   let textAreaElement = document.querySelector('body > div > textarea');
+   let checkoutElement = document.querySelector('.checkout');
+   checkoutElement.addEventListener('click', checkoutFunc);
+
+   function addFunc(e) {
+
+      if (e.target.className == 'add-product') {
+         let parentElementDiv = Array.from(e.target.parentElement.parentElement.children);
+         let currentPrice = 0;
+         let currentProductName = '';
+
+         for (let el of parentElementDiv) {
+
+            if (el.className == 'product-line-price') {
+               currentPrice = Number(el.textContent);
+               totalPrice += Number(currentPrice);
+               console.log(currentPrice);
+            }
+
+            if (el.className == 'product-details') {
+               let currentParentEl = Array.from(el.children);
+               for (let el2 of currentParentEl) {
+                  if (el2.className == 'product-title') {
+                     currentProductName = el2.textContent;
+                     allProducts.add(currentProductName);
+                     console.log(currentProductName);
+                  }
+               }
+            }
+         }
+
+         textAreaElement.value += `Added ${currentProductName} for ${(currentPrice).toFixed(2)} to the cart.\n`;
+
+      }
+   }
+
+   function checkoutFunc() {
+      for (let product of allProducts) {
+         allProductsArr.push(product);
+      }
+
+      textAreaElement.value += `You bought ${allProductsArr.join(', ')} for ${(totalPrice).toFixed(2)}.`
+      productsElements.removeEventListener('click', addFunc);
+      checkoutElement.removeEventListener('click', checkoutFunc);
+
+   }
 }
