87a441b461dc2fe5ae2c50818c9c1dbc8a769695
Peter Zhechev <petar_vn7@abv.bg>
2023-05-18 19:34:02 +0300

    Update script.js

--- 10_dom_manipulation_and_events_(exercise)/01. Sections/script.js
+++ 10_dom_manipulation_and_events_(exercise)/01. Sections/script.js
@@ -1,3 +1,21 @@
 function create(words) {
-   console.log('TODO:...');
+
+   let contentDivElement = document.getElementById('content');
+   
+   for (let word of words) {
+      let currentDivElement = document.createElement('div');
+      let currentPElement = document.createElement('p');
+      currentPElement.style.display = 'none';
+      contentDivElement.addEventListener('click', addPFunc)
+      currentPElement.textContent = word;
+
+      contentDivElement.appendChild(currentDivElement);
+      currentDivElement.appendChild(currentPElement);
+
+   }
+   
+   function addPFunc(e) {
+      e.target.children[0].style.display = '';
+      console.log(e.target.children[0].style.display);
+   }
 }
