6923433e31ac3b7c6599352bdb6aa36232ffbe4d
Peter Zhechev <petar_vn7@abv.bg>
2023-05-21 20:05:33 +0300

    Update 06_from_json_to_html_table.js

--- 05_objects_and_composition_(lab)/06_from_json_to_html_table.js
+++ 05_objects_and_composition_(lab)/06_from_json_to_html_table.js
@@ -55,7 +55,7 @@ function fromJsonToHtmlTable(input) {
 // ));
 
 console.log(fromJsonToHtmlTable(`[
-{"Name":"<Pesho", "Score":4, " Grade":8},
+{"Name":"Pesho", "Score":4, " Grade":8},
 {"Name":"Gosho", "Score":5, " Grade":8},
 {"Name":"Angel", "Score":5.50, " Grade":10}
 ]`));
