583ce7508260a93844981e188c623cbc5663a538
Peter Zhechev <petar_vn7@abv.bg>
2023-05-22 16:20:19 +0300

    Update app.js

--- 10_dom_manipulation_and_events_(exercise)/03. Locked-Profile/app.js
+++ 10_dom_manipulation_and_events_(exercise)/03. Locked-Profile/app.js
@@ -1,3 +1,19 @@
 function lockedProfile() {
-    console.log('TODO...')
+    let buttons = Array.from(document.querySelectorAll('#container #main .profile'));
+
+    for (let el of buttons) {
+        el.querySelector('button').addEventListener('click', showFunc);
+    }
+
+    function showFunc(e) {
+        if (e.currentTarget.parentNode.querySelector('input:checked').value === 'unlock') {
+            if (e.target.textContent === 'Show more') {
+                e.target.previousElementSibling.style.display = 'block';
+                e.target.textContent = 'Hide it';
+            } else if (e.target.textContent === 'Hide it') {
+                e.target.previousElementSibling.style.display = 'none';
+                e.target.textContent = 'Show more';
+            }
+        }
+    }
 }
