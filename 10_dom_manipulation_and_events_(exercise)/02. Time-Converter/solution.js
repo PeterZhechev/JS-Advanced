04a384eaf2265e0cc569f5b9ce383cbb16583d7c
Peter Zhechev <petar_vn7@abv.bg>
2023-05-22 13:30:12 +0300

    Update solution.js

--- 10_dom_manipulation_and_events_(exercise)/02. Time-Converter/solution.js
+++ 10_dom_manipulation_and_events_(exercise)/02. Time-Converter/solution.js
@@ -1,4 +1,41 @@
 function attachEventsListeners() {
 
-    console.log('TODO:...');
+    let daysInputElement = document.getElementById('days');
+    let hoursInputElement = document.getElementById('hours');
+    let minutesInputElement = document.getElementById('minutes');
+    let secondsInputElement = document.getElementById('seconds');
+
+    let daysButtonElement = document.getElementById('daysBtn');
+    let hoursButtonElement = document.getElementById('hoursBtn');
+    let minutesButtonElement = document.getElementById('minutesBtn');
+    let secondsButtonElement = document.getElementById('secondsBtn');
+
+    daysButtonElement.addEventListener('click', daysFunc);
+    hoursButtonElement.addEventListener('click', hoursFunc);
+    minutesButtonElement.addEventListener('click', minutesFunc);
+    secondsButtonElement.addEventListener('click', secondsFunc);
+
+    function daysFunc() {
+        hoursInputElement.value = Number(daysInputElement.value) * 24;
+        minutesInputElement.value = Number(daysInputElement.value) * 1440;
+        secondsInputElement.value = Number(daysInputElement.value) * 86400;
+    }
+
+    function hoursFunc() {
+        daysInputElement.value = Number(hoursInputElement.value) / 24;
+        minutesInputElement.value = Number(hoursInputElement.value) * 60;
+        secondsInputElement.value = Number(hoursInputElement.value) * 3600;
+    }
+
+    function minutesFunc() {
+        daysInputElement.value = Number(minutesInputElement.value) / 1440;
+        hoursInputElement.value = Number(minutesInputElement.value) / 60;
+        secondsInputElement.value = Number(minutesInputElement.value) * 60;
+    }
+
+    function secondsFunc() {
+        daysInputElement.value = Number(secondsInputElement.value) / 86400;
+        hoursInputElement.value = Number(secondsInputElement.value) / 3600;
+        minutesInputElement.value = Number(secondsInputElement.value) / 60;
+    }
 }
