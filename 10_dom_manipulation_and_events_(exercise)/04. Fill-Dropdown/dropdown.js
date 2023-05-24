228cb28b88ca29324eff3332786c3d0925253c5d
Peter Zhechev <petar_vn7@abv.bg>
2023-05-24 09:24:30 +0300

    Update dropdown.js

--- 10_dom_manipulation_and_events_(exercise)/04. Fill-Dropdown/dropdown.js
+++ 10_dom_manipulation_and_events_(exercise)/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,13 @@
 function addItem() {
-    console.log('TODO:...');
+    let textInputElement = document.getElementById('newItemText');
+    let valueInputElement = document.getElementById('newItemValue');
+
+    let newOptionElement = document.createElement('option');
+    newOptionElement.textContent = textInputElement.value;
+    newOptionElement.value = valueInputElement.value;
+    let menuElement = document.getElementById('menu');
+    menuElement.appendChild(newOptionElement);
+
+    textInputElement.value = '';
+    valueInputElement.value = '';
 }
