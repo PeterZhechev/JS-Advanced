b2b291ad34b847f9e424a4852e6a3052020df4e7
Peter Zhechev <petar_vn7@abv.bg>
2023-05-27 16:14:56 +0300

    added new tasks

---
+++ 11_advanced_functions_(lab)/02_add.js
@@ -0,0 +1,13 @@
+function solution(num) {
+    return function (num2) {
+        return num + num2;
+    }
+}
+
+// let add5 = solution(5);
+// console.log(add5(2));
+// console.log(add5(3));
+
+let add7 = solution(7);
+console.log(add7(2));
+console.log(add7(3));
---
+++ 12_advanced_functions_(exercise)/04_breakfast_robot.js
@@ -0,0 +1,7 @@
+function solution(params) {
+
+}
+
+let manager = solution();
+console.log(manager("restock flavour 50")); // Success
+console.log(manager("prepare lemonade 4")); // Error: not enough carbohydrate in stock
