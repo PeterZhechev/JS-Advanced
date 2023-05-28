8eb84dcf74338ee67f87b3511a45e43eb8105f3a
Peter Zhechev <petar_vn7@abv.bg>
2023-05-28 21:58:49 +0300

    Update app.js

--- 12_advanced_functions_(exercise)/07. Simple Calculator/app.js
+++ 12_advanced_functions_(exercise)/07. Simple Calculator/app.js
@@ -1,7 +1,26 @@
 function calculator() {
-    // TODO:
+    let selectorOneElement = document.getElementById('num1');
+    let selectorTwoElement = document.getElementById('num2');
+    let resultFieldElement = document.getElementById('result');
+
+    let calculate = {
+        init() {
+            selectorOneElement = document.getElementById('num1');
+            selectorTwoElement = document.getElementById('num2');
+            resultFieldElement = document.getElementById('result');
+        },
+
+        add() {
+            resultFieldElement.value = Number(selectorOneElement.value) + Number(selectorTwoElement.value);
+        },
+
+        subtract() {
+            resultFieldElement.value = Number(selectorOneElement.value) - Number(selectorTwoElement.value);
+        },
+    }
+
+    return calculate;
 }
 
-
-
-
+const calculate = calculator ();
+calculate.init ('#num1', '#num2', '#result');
