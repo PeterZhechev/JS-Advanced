a4a18d1726ef604eab21997943fe8941865c7c3f
Peter Zhechev <petar_vn7@abv.bg>
2023-06-02 17:04:39 +0300

    added new tests

--- 13_unit_testing_and_error_handling_(lab_and_exercise)/unit_testing_and_error_handling_(exercise)/06.Dynamic-Validation/app.js
+++ 13_unit_testing_and_error_handling_(lab_and_exercise)/unit_testing_and_error_handling_(exercise)/06.Dynamic-Validation/app.js
@@ -1,3 +1,16 @@
 function validate() {
-    // TODO
+    let inputEmailElement = document.getElementById('email');
+    let pattern = /[a-z]+@[a-z]+\.[a-z]+/g
+
+    inputEmailElement.addEventListener('change', inputEmailFunc);
+
+    function inputEmailFunc(e) {
+
+        let matched = e.target.value.match(pattern);
+        if (matched == null) {
+            inputEmailElement.classList.add('error');
+        } else {
+            inputEmailElement.classList.remove('error');
+        }
+    }
 }
