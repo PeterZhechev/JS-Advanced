e95c6a2540b58a114c0370cf4905534288ee181c
Peter Zhechev <petar_vn7@abv.bg>
2023-06-02 15:07:10 +0300

    update task

--- 13_unit_testing_and_error_handling_(lab_and_exercise)/unit_testing_and_error_handling_(exercise)/05.Notification/app.js
+++ 13_unit_testing_and_error_handling_(lab_and_exercise)/unit_testing_and_error_handling_(exercise)/05.Notification/app.js
@@ -1,4 +1,10 @@
 function notify(message) {
-  console.log('todo')
-  // TODO:
+  let divElement = document.getElementById('notification');
+  divElement.textContent = message;
+  divElement.style.display = 'block';
+  divElement.addEventListener('click', hideFunc);
+
+  function hideFunc() {
+    divElement.style.display = 'none';
+  }
 }
