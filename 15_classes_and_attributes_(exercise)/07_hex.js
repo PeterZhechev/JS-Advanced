d11cc810c0afece4bbba0cb6434bed4e4ebd7b19
Peter Zhechev <petar_vn7@abv.bg>
2023-06-06 18:07:17 +0300

    Create 07_hex.js

---
+++ 15_classes_and_attributes_(exercise)/07_hex.js
@@ -0,0 +1,36 @@
+class Hex {
+    constructor(value) {
+        this.value = value;
+    }
+
+    valueOf() {
+        return this.value;
+    }
+
+    toString() {
+        return `0x${this.value.toString(16).toUpperCase()}`;
+    }
+
+    plus(number) {
+        let newObj = new Hex(this.value + number.value);
+        return newObj;
+    }
+
+    minus(number) {
+        let newObj = new Hex(this.value - number.value);
+        return newObj;
+    }
+
+    parse(str) {
+        return parseInt(str, 16);
+    }
+}
+
+let FF = new Hex(255);
+console.log(FF.toString());
+FF.valueOf() + 1 == 256;
+let a = new Hex(10);
+let b = new Hex(5);
+console.log(a.plus(b).toString());
+console.log(a.plus(b).toString() === '0xF');
+console.log(FF.parse('AAA'));
